a7f8fa7dccc619bb190dc91e8bee3b6f59655b5a
senthilmurugan41 <54075604+senthilmurugan41@users.noreply.github.com>
2019-08-18 14:27:19 +0530

    Create kswap.cpp

---
+++ kswap.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<algorithm>
+#include<math.h>
+using namespace std;
+int main()
+{
+  int n,k;
+  cin>>n>>k;
+  int * a= new int[n];
+  for(int i=0;i<n;i++)
+  {
+      cin>>a[i];
+  }
+  for(int i=0;i<k;i++)
+  {
+      swap(a[i],a[i+1]);
+  }
+  for(int i=0;i<n;i++)
+  cout<<a[i<<" ";
+}
