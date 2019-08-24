457969eeea0f9386f0c7fb7b4a9a78844a2e51d3
senthilmurugan41 <54075604+senthilmurugan41@users.noreply.github.com>
2019-08-24 21:52:29 +0530

    Create permutation.cpp

---
+++ permutation.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+int main()
+{
+    string s;
+    cin>>s;
+    sort(s.begin(),s.end());
+    do{
+        cout<<s<<endl;
+    }while(next_permutation(s.begin(),s.end()));
+}
