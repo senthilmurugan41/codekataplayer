4015a2785d55efb4e5ebc03961b04cf5623f470c
senthilmurugan41 <54075604+senthilmurugan41@users.noreply.github.com>
2019-08-25 21:24:32 +0530

    Create nocheck.cpp

---
+++ nocheck.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,n1,f=1;
+    cin>>n>>n1;
+    int * a=new int[n];
+    int * b=new int[n1];
+    for(int i=0;i<n;i++)
+        cin>>a[i];
+    for(int j=0;j<n1;j++)
+        cin>>b[j];
+    for(int i=0;i<n1;i++)
+    {
+        f=1;
+        for(int j=0;j<n;j++)
+        {
+            if(a[i]==b[j])
+            {
+               f=0;
+               break;
+            }
+        }
+    }
+    if(f!=0)
+        cout<<"NO";
+    else
+        cout<<"YES";
+}
